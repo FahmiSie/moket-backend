f675f9b48b8e16ede48d2339df8ddace3e06123b
Fahmi Aqila Maulana <fahmi_maulana_33rpl@student.smktelkom-mlg.sch.id>
2026-08-04 18:48:55 +0700

    feat(api): define auth api contracts and routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController; 
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Public Routes (Bisa diakses tanpa login)
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected Routes (Hanya bisa diakses jika menyertakan Token Sanctum yang valid)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+});
